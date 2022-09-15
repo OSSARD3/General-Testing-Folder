d23a7a1176856d34283a0907cc839730811a4185
Holst <93986125+nvHolst@users.noreply.github.com>
2022-09-15 13:11:07 +0300

    Implement: new page from mockup

---
+++ src/pages/LandscapePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-drawer v-model="left" side="left" width="300">
+      <div v-for="index in 10" :key="index">
+        test
+      </div>
+    </q-drawer>
+    <q-page-container>
+      <q-page class="background">
+        <div class="q-pa-md">
+          <q-btn :loading="progress[2].loading" :percentage="progress[2].percentage" dark-percentage unelevated
+            color="blue-10" text-color="blue-grey-11" @click="startComputing(2)" icon="cloud_upload"
+            style="width: 100px;" />
+        </div>
+      </q-page>
+
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const left = ref(true)
+</script>
