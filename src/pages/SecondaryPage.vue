09de717ef4bf28cce6e76f582679c59b9fecb0b9
Holst <93986125+nvHolst@users.noreply.github.com>
2022-09-15 09:49:34 +0300

    commit on 'master'

--- src/pages/SecondaryPage.vue
+++ src/pages/SecondaryPage.vue
@@ -17,7 +17,7 @@
       </div>
 
       <div class="col-12 flex flex-center q-mt-xl">
-        <q-btn round color="black" icon="chevron_left" />
+        <q-btn round color="black" icon="chevron_left" @click="$router.push('/primary')" />
       </div>
     </div>
 
